0e3516f07bbb6083ae03bd3ccd03ba9b3a038a1b
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-12-03 18:06:12 -0500

    Adding useNavigate to login page

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        navigate('/login', {
+            replace: true,
+        });
+    };
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <Link className="navbar-brand" to="/">
@@ -50,7 +58,12 @@ export const Navbar = () => {
                     <span className="nav-item nav-link text-primary">
                         Edwin
                     </span>
-                    <button className="btn nav-item nav-link">Logout</button>
+                    <button
+                        className="btn nav-item nav-link"
+                        onClick={handleLogout}
+                    >
+                        Logout
+                    </button>
                 </ul>
             </div>
         </nav>
